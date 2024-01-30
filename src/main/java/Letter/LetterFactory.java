a14f05f174d72c895b34bb727ab40afaf068c545
Ann-Sophie St-Amand <anso.st.amand02@gmail.com>
2024-01-30 14:27:02 -0500

    fait le A

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -17,7 +17,23 @@ public final class LetterFactory {
      * @return BaseShape containing the letter A
      */
     public static BaseShape create_A()  {
-        return null;
+        BaseShape letterA = new BaseShape();
+
+        Rectangle left = new Rectangle(halfStripeThickness, maxHeight);
+        left.rotate(left.getCoords(), (11.5*Math.PI) / 6);
+        left.translate(left.getCoords(), new Point2d(30.0, 0.0));
+        letterA.addAll(left.getCoords());
+
+        Rectangle right = new Rectangle(halfStripeThickness, maxHeight);
+        right.rotate(right.getCoords(), Math.PI / 9);
+        right.translate(right.getCoords(), new Point2d(-30.0, 0.0));
+        letterA.addAll(right.getCoords());
+
+        Rectangle middle = new Rectangle(halfMaxWidth + 10, halfStripeThickness);
+        middle.translate(middle.getCoords(), new Point2d(0.0, -2.0));
+        letterA.addAll(middle.getCoords());
+
+        return letterA;
     }
 
     /** TODO
